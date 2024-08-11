bbe4cb976fdb37833e6591de702388536a70bb51
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-08-11 14:38:24 -0400

    Github login

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,14 +1,18 @@
 'use client';
 import Image from "next/image";
 
-const Avatar = () => {
+interface AvatarProps {
+    src?: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
   return (
     <Image 
         alt='avatar'
         className="rounded-full"
         height="30"
         width="30"
-        src="/user.png"
+        src={src || "/user.png"}
     />
   )
 }
