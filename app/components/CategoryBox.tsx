80bdeac42c83e0a053884a8a8d47b80475f209f1
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-08-13 17:17:23 -0400

    Category URL logic

--- app/components/CategoryBox.tsx
+++ app/components/CategoryBox.tsx
@@ -2,6 +2,8 @@
 
 import { useRouter, useSearchParams } from 'next/navigation';
 import { IconType } from 'react-icons';
+import { useCallback } from 'react';
+import qs from 'query-string'
 
 
 interface CategoryBoxProps {
@@ -14,8 +16,35 @@ interface CategoryBoxProps {
 const CategoryBox: React.FC<CategoryBoxProps> = ({icon: Icon, label, selected}) => {
     const router = useRouter();
     const params = useSearchParams();
+
+    const handleClick = useCallback(() => {
+        let currentQuery = {};
+        if (params) { // if there are parameters 
+            currentQuery = qs.parse(params.toString()); // creating an object out of current parameters
+        }
+
+        const updatedQuery: any = { 
+            ...currentQuery, // copy the parameters of currentQuery
+            category: label, // add a 'category' label to the query
+        }
+
+        if(params?.get('category') === label) {
+            delete updatedQuery.category; // if the category is already selected, remove it. This makes it possible to deselect the category
+        }
+
+        const url = qs.stringifyUrl({ // generate a new url with the updated query
+            url: '/',
+            query: updatedQuery,
+        }, {skipNull: true});
+
+        router.push(url);
+    }, [label, params, router]);
+
+
   return (
-    <div className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer 
+    <div 
+        onClick={handleClick}
+        className={`flex flex-col items-center justify-center gap-2 p-3 border-b-2 hover:text-neutral-800 transition cursor-pointer 
         ${selected ? 'border-b-neutral-800': 'border-transparent'} 
         ${selected ? 'text-neutral-800': 'text-neutral-500'} 
     `}
--- app/components/navbar/Categories.tsx
+++ app/components/navbar/Categories.tsx
@@ -3,9 +3,10 @@
 
 import Container from '../Container'
 import { FaSunPlantWilt } from 'react-icons/fa6'
-import { GiGreenhouse } from 'react-icons/gi'
+import { GiGreenhouse, GiFruitTree, GiGrapes, GiLilyPads } from 'react-icons/gi'
 import { PiBarn } from 'react-icons/pi'
 import CategoryBox from '../CategoryBox'
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export const categories = [
     {
@@ -20,24 +21,51 @@ export const categories = [
         label: 'Farm',
         icon: PiBarn,
     },
+    {
+        label: 'Orchard',
+        icon: GiFruitTree,
+    },
+    {
+        label: 'Vineyard',
+        icon: GiGrapes,
+    },
+    {
+        label: 'Aquaponic',
+        icon: GiLilyPads,
+    },
+    
 
 ]
 
 const Categories = () => {
-  return (
-    <Container>
-        <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
-            {categories.map((item) => (
-                <CategoryBox 
-                    key={item.label}
-                    label={item.label}
-                    icon={item.icon}
-                />
-            ))}
-        </div>
-           
-    </Container>
-  )
+    const params = useSearchParams();
+    const category = params?.get('category'); 
+    const pathname = usePathname();
+
+    const isMainPage = pathname === '/';
+
+    if (!isMainPage) {
+        return null;
+    }
+
+
+
+
+    return (
+        <Container>
+            <div className="pt-4 flex flex-row items-center justify-between overflow-x-auto">
+                {categories.map((item) => (
+                    <CategoryBox 
+                        key={item.label}
+                        label={item.label}
+                        selected={category === item.label}
+                        icon={item.icon}
+                    />
+                ))}
+            </div>
+            
+        </Container>
+    )
 }
 
 export default Categories
