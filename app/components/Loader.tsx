d56a8ef6c40912943d5cf5171eb7ece78779fa22
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-08-26 08:26:54 -0400

    loader color error

--- app/components/Loader.tsx
+++ app/components/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => {
         <div className="h-[70vh] flex flex-col justify-center items-center">
             <PuffLoader 
                 size={100}
-                color="airbnb"
+                color="#46a807"
             />
         </div>
     )
