09fc770d44e0ae75b7e749ea7126c311f74c240f
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-08-28 12:10:26 -0400

    ES lint disabel

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,18 @@
 //TODO: update this to use remotePatterns
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        'avatars.githubusercontent.com', 
-        'lh3.googleusercontent.com', 
-        'res.cloudinary.com'
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: [
+      'avatars.githubusercontent.com', 
+      'lh3.googleusercontent.com', 
+      'res.cloudinary.com'
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
+
+export default nextConfig;
