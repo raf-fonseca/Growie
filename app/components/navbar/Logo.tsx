7b4b67ef5cd60252c6a719f82841e0e01fc164ae
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-10-09 20:56:32 -0400

    new logo

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,21 +1,20 @@
-'use client';
+"use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation"; // Using this to redirect user to home page when logo is clicked
 
 const Logo = () => {
     const router = useRouter();
-  return (
-    <Image 
-        onClick={() => router.push('/')}
-        alt='logo'
-        className="hidden md:block cursor-pointer"
-        height="175"
-        width="175"
-        src="/logoMedium.png"
-    
-    />
-  )
-}
+    return (
+        <Image
+            onClick={() => router.push("/")}
+            alt="logo"
+            className="hidden md:block cursor-pointer"
+            height="175"
+            width="175"
+            src="/logoMedium.jpeg"
+        />
+    );
+};
 
 export default Logo;
