21a0e6af7cd93ef0dbb8444802a13b111f0bd76a
Rafael Fonseca <rafael.silvanofonseca@gmail.com>
2024-12-08 17:16:18 -0500

    cleanup

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,20 +1,19 @@
 // This is a direct communicating with the db through a server conponent
 import { getServerSession } from "next-auth/next";
 
-import {authOptions} from "@/pages/api/auth/[...nextauth]";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
     return await getServerSession(authOptions);
 }
 
-
 export default async function getCurrentUser() {
     try {
         const session = await getSession();
 
         // if there is no session or user email, return null
-        if(!session?.user?.email){
+        if (!session?.user?.email) {
             return null;
         }
 
@@ -22,10 +21,10 @@ export default async function getCurrentUser() {
         const currentUser = await prisma.user.findUnique({
             where: {
                 email: session.user.email as string,
-            }
+            },
         });
 
-        if (!currentUser){
+        if (!currentUser) {
             return null;
         }
 
@@ -33,10 +32,11 @@ export default async function getCurrentUser() {
             ...currentUser,
             createdAt: currentUser.createdAt.toISOString(),
             updatedAt: currentUser.updatedAt.toISOString(),
-            emailVerified: currentUser.emailVerified ? currentUser.emailVerified.toISOString() : null
-        }
-    } catch (error: any){
+            emailVerified: currentUser.emailVerified
+                ? currentUser.emailVerified.toISOString()
+                : null,
+        };
+    } catch (error: any) {
         return null;
     }
-
 }
